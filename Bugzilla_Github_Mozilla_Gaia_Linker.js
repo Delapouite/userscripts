1f12a0a190711ce0662099e29976cf6ec8ebdf4b
Delapouite <delapouite@gmail.com>
2013-05-27 13:14:56 +0200

    added Bugzilla - Github - Mozilla Gaia Linker

---
+++ Bugzilla_Github_Mozilla_Gaia_Linker.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        Bugzilla - Github - Mozilla Gaia Linker
+// @namespace   http://userscripts.org/users/delapouite
+// @description Create links from Github issues to their corresponding Bugzilla entries for the Mozilla Gaia project
+// @include     https://github.com/mozilla-b2g/gaia/*
+// @version     1
+// ==/UserScript==
+
+// grab the bug Id in the title
+var discussionTitle = document.getElementsByClassName('discussion-topic-title');
+if (discussionTitle) {
+	discussionTitle = discussionTitle[0];
+	matches = discussionTitle.textContent.match('^Bug (\\d*) - (.*)')
+	if (matches[1]) {
+		// create link
+		discussionTitle.innerHTML = '<a href="https://bugzilla.mozilla.org/show_bug.cgi?id=' + matches[1] + '">Bug ' + matches[1] + '</a>';
+		discussionTitle.innerHTML += ' - ' + matches[2];
+	}
+}
