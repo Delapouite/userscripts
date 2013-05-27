a6a6fff1c3ee3d32ebc6d26716f50e5d1447f3e1
Delapouite <delapouite@gmail.com>
2013-05-27 14:18:42 +0200

    started work on notifications page

--- Bugzilla_Github_Mozilla_Gaia_Linker.js
+++ Bugzilla_Github_Mozilla_Gaia_Linker.js
@@ -6,15 +6,31 @@
 // @version     1
 // ==/UserScript==
 
-// grab the bug Id in the title
+var re = /^\[?bug (\d*)\]? (.*)/i;
+
+// View issue page : grab the bug Id in the title
 var discussionTitle = document.getElementsByClassName('discussion-topic-title');
-if (discussionTitle) {
+if (discussionTitle.length !== 0) {
 	discussionTitle = discussionTitle[0];
-	var re = /^\[?bug (\d*)\]? (.*)/i;
 	var matches = discussionTitle.textContent.match(re);
 	if (matches[1]) {
 		// create link
 		discussionTitle.innerHTML = '<a href="https://bugzilla.mozilla.org/show_bug.cgi?id=' + matches[1] + '">Bug ' + matches[1] + '</a>';
 		discussionTitle.innerHTML += ' ' + matches[2];
 	}
+}
+
+// Notifications page
+var notifications = document.querySelectorAll('.js-notification h4');
+if (notifications.length !== 0) {
+	Array.prototype.forEach.call(notifications, function (notification) {
+		// move icons out of the way
+		var icon = notification.getElementsByClassName('type-icon')[0];
+		var link = notification.getElementsByTagName('a')[0];
+		var clonedIcon = icon.cloneNode();
+		clonedIcon.style.margin = '12px 6px 12px 0';
+		clonedIcon.style.cssFloat = 'left';
+		notification.insertBefore(clonedIcon, link);
+		icon.remove();
+	});
 }
