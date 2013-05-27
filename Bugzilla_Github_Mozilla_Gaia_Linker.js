c73a35f7dec316412e3c135dbc6bf7afec2646e2
Delapouite <delapouite@gmail.com>
2013-05-27 18:09:35 +0200

    added notifications and issues lists linkification

--- Bugzilla_Github_Mozilla_Gaia_Linker.js
+++ Bugzilla_Github_Mozilla_Gaia_Linker.js
@@ -7,17 +7,36 @@
 // ==/UserScript==
 
 var re = /^\[?bug (\d*)\]? (.*)/i;
+var endPoint = 'https://bugzilla.mozilla.org/show_bug.cgi?id=';
 
-// View issue page : grab the bug Id in the title
+// Issue page
 var discussionTitle = document.getElementsByClassName('discussion-topic-title');
 if (discussionTitle.length !== 0) {
 	discussionTitle = discussionTitle[0];
-	var matches = discussionTitle.textContent.match(re);
-	if (matches[1]) {
+	var matches = discussionTitle.textContent.trim().match(re);
+	if (matches && matches[1]) {
 		// create link
-		discussionTitle.innerHTML = '<a href="https://bugzilla.mozilla.org/show_bug.cgi?id=' + matches[1] + '">Bug ' + matches[1] + '</a>';
+		discussionTitle.innerHTML = '<a href="' + endPoint + matches[1] + '">Bug ' + matches[1] + '</a>';
 		discussionTitle.innerHTML += ' ' + matches[2];
 	}
+}
+
+function splitLink(link) {
+	var matches = link.textContent.trim().match(re);
+	if (!matches || !matches[1]) {
+		return;
+	}
+	// append bugzilla link
+	var bugzillaLink = document.createElement('a');
+	bugzillaLink.href = endPoint + matches[1];
+	bugzillaLink.textContent = 'Bug ' + matches[1];
+	bugzillaLink.style.cssFloat = 'left';
+	bugzillaLink.style.color = '#4183c4';
+	bugzillaLink.style.marginRight = '6px';
+	link.parentNode.insertBefore(bugzillaLink, link);
+
+	// shorten the original link
+	link.textContent = matches[2];
 }
 
 // Notifications page
@@ -32,5 +51,15 @@ if (notifications.length !== 0) {
 		clonedIcon.style.cssFloat = 'left';
 		notification.insertBefore(clonedIcon, link);
 		icon.remove();
+
+		splitLink(link);
+	});
+}
+
+// Issues page
+var issues = document.querySelectorAll('.issue-list-item h4');
+if (issues.length !== 0) {
+	Array.prototype.forEach.call(issues, function (issue) {
+		splitLink(issue.getElementsByTagName('a')[0]);
 	});
 }
