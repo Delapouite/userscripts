31e1928eb5c4d71e34eba6455f0161de531a17e7
Delapouite <delapouite@gmail.com>
2013-06-25 19:08:31 +0200

    added last update date in history button

--- MDN_l10n_FR.user.js
+++ MDN_l10n_FR.user.js
@@ -4,33 +4,44 @@
 // @description Add the number of available languages on the dropdown toggle itself
 // @include     https://developer.mozilla.org/en-US/docs/*
 // @include     https://developer.mozilla.org/fr/docs/*
-// @version     2
+// @version     3
 // ==/UserScript==
 
 document.addEventListener('DOMContentLoaded', function() {
-	var languagesToggle = document.querySelector('#tools > li:nth-child(2) a');
+	var translationsToggle = document.querySelector('#tools > li:nth-child(2) a');
 	var translations = document.querySelectorAll('#translations a');
 	var articleLinks = document.querySelectorAll('#wikiArticle a');
+	var historyLink = document.querySelector('.page-history a');
+	var lastUpdate = document.querySelector('#doc-contributors time').textContent;
+
 	// remove 'Add translation' link from count
-	languagesToggle.textContent += ' (' + (translations.length - 1) + ')';
+	translationsToggle.textContent += ' (' + (translations.length - 1) + ')';
+
+	function addTranslationShortcut(translation) {
+		// remove arrow
+		translation.style.backgroundImage = 'none';
+		var li = document.createElement('li');
+		li.className = 'menu';
+		li.appendChild(translation);
+		document.getElementById('tools').appendChild(li);
+	}
+
 	for (var i = 0; i < translations.length; i++) {
 		var translation = translations[i];
-		// add a FR direct access in the nav bar
-		if (translation.textContent.match('Français')) {
-			var frLink = document.createElement('a');
-			frLink.textContent = 'FR';
-			frLink.href = translation.href;
-			var li = document.createElement('li');
-			li.className = 'menu';
-			li.appendChild(frLink);
-			document.getElementById('tools').appendChild(li);
+		// add a FR or US direct access in the nav bar
+		if (translation.textContent.match('Français|English')) {
+			addTranslationShortcut(translation);
 		}
 	}
-	// turn FR links in green in the content
+	// turn FR links in green in the content if they point to another FR page
 	for (var i = 0; i < articleLinks.length; i++) {
 		var articleLink = articleLinks[i];
 		if (articleLink.href.match(/\/fr\//)) {
-			articleLink.style.color = 'green';
+			articleLink.style.background = 'rgba(0, 255, 0, 0.4)';
 		}
+	}
+	// TODO : keep the book icon
+	if (historyLink) {
+		historyLink.textContent += ' - ' + lastUpdate;
 	}
 });
