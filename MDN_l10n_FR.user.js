8f6b9e78f60a001c2ccba02c8eabfed32bc91b86
Delapouite <delapouite@gmail.com>
2013-06-28 17:54:53 +0200

    made MDN script work on root pages

--- MDN_l10n_FR.user.js
+++ MDN_l10n_FR.user.js
@@ -2,37 +2,42 @@
 // @name        MDN - l10n - FR
 // @namespace   http://userscripts.org/users/delapouite
 // @description Add the number of available languages on the dropdown toggle itself
-// @include     https://developer.mozilla.org/en-US/docs/*
-// @include     https://developer.mozilla.org/fr/docs/*
+// @include     https://developer.mozilla.org/en-US/docs*
+// @include     https://developer.mozilla.org/fr/docs*
 // @updateURL   https://github.com/Delapouite/userscripts/raw/master/MDN_l10n_FR.user.js
 // @downloadURL https://github.com/Delapouite/userscripts/raw/master/MDN_l10n_FR.user.js
-// @version     3.1
+// @version     3.2
 // ==/UserScript==
 
 document.addEventListener('DOMContentLoaded', function() {
 	var translationsToggle = document.querySelector('#tools > li:nth-child(2) a');
 	var translations = document.querySelectorAll('#translations a');
-	var articleLinks = document.querySelectorAll('#wikiArticle a');
+	var articleLinks = document.querySelectorAll('#content a');
 	var historyLink = document.querySelector('.page-history a');
-	var lastUpdate = document.querySelector('#doc-contributors time').textContent;
-
-	// remove 'Add translation' link from count
-	translationsToggle.textContent += ' (' + (translations.length - 1) + ')';
-
-	function addTranslationShortcut(translation) {
-		// remove arrow
-		translation.style.backgroundImage = 'none';
-		var li = document.createElement('li');
-		li.className = 'menu';
-		li.appendChild(translation);
-		document.getElementById('tools').appendChild(li);
+	var lastUpdate = document.querySelector('#doc-contributors time');
+	if (lastUpdate) {
+		lastUpdate = lastUpdate.textContent;
 	}
 
-	for (var i = 0; i < translations.length; i++) {
-		var translation = translations[i];
-		// add a FR or US direct access in the nav bar
-		if (translation.textContent.match('Français|English')) {
-			addTranslationShortcut(translation);
+	if (translationsToggle) {
+		// remove 'Add translation' link from count
+		translationsToggle.textContent += ' (' + (translations.length - 1) + ')';
+
+		function addTranslationShortcut(translation) {
+			// remove arrow
+			translation.style.backgroundImage = 'none';
+			var li = document.createElement('li');
+			li.className = 'menu';
+			li.appendChild(translation);
+			document.getElementById('tools').appendChild(li);
+		}
+
+		for (var i = 0; i < translations.length; i++) {
+			var translation = translations[i];
+			// add a FR or US direct access in the nav bar
+			if (translation.textContent.match('Français|English')) {
+				addTranslationShortcut(translation);
+			}
 		}
 	}
 	// turn FR links in green in the content if they point to another FR page
