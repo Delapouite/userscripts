ec9b63d7a7ce6d519d4952d6ec5d3824c2f9c88f
Delapouite <delapouite@gmail.com>
2013-07-03 22:04:57 +0200

    added dashboard link to MDN

--- MDN_l10n_FR.user.js
+++ MDN_l10n_FR.user.js
@@ -2,42 +2,38 @@
 // @name        MDN - l10n - FR
 // @namespace   http://userscripts.org/users/delapouite
 // @description Add the number of available languages on the dropdown toggle itself
-// @include     https://developer.mozilla.org/en-US/docs*
-// @include     https://developer.mozilla.org/fr/docs*
+// @include     https://developer.mozilla.org/en-US/docs/*
+// @include     https://developer.mozilla.org/fr/docs/*
 // @updateURL   https://github.com/Delapouite/userscripts/raw/master/MDN_l10n_FR.user.js
 // @downloadURL https://github.com/Delapouite/userscripts/raw/master/MDN_l10n_FR.user.js
-// @version     3.2
+// @version     4
 // ==/UserScript==
 
 document.addEventListener('DOMContentLoaded', function() {
 	var translationsToggle = document.querySelector('#tools > li:nth-child(2) a');
 	var translations = document.querySelectorAll('#translations a');
-	var articleLinks = document.querySelectorAll('#content a');
+	var articleLinks = document.querySelectorAll('#wikiArticle a');
 	var historyLink = document.querySelector('.page-history a');
-	var lastUpdate = document.querySelector('#doc-contributors time');
-	if (lastUpdate) {
-		lastUpdate = lastUpdate.textContent;
+	var lastUpdate = document.querySelector('#doc-contributors time').textContent;
+	var userState = document.querySelector('.user-state');
+
+	// remove 'Add translation' link from count
+	translationsToggle.textContent += ' (' + (translations.length - 1) + ')';
+
+	function addTranslationShortcut(translation) {
+		// remove arrow
+		translation.style.backgroundImage = 'none';
+		var li = document.createElement('li');
+		li.className = 'menu';
+		li.appendChild(translation);
+		document.getElementById('tools').appendChild(li);
 	}
 
-	if (translationsToggle) {
-		// remove 'Add translation' link from count
-		translationsToggle.textContent += ' (' + (translations.length - 1) + ')';
-
-		function addTranslationShortcut(translation) {
-			// remove arrow
-			translation.style.backgroundImage = 'none';
-			var li = document.createElement('li');
-			li.className = 'menu';
-			li.appendChild(translation);
-			document.getElementById('tools').appendChild(li);
-		}
-
-		for (var i = 0; i < translations.length; i++) {
-			var translation = translations[i];
-			// add a FR or US direct access in the nav bar
-			if (translation.textContent.match('Français|English')) {
-				addTranslationShortcut(translation);
-			}
+	for (var i = 0; i < translations.length; i++) {
+		var translation = translations[i];
+		// add a FR or US direct access in the nav bar
+		if (translation.textContent.match('Français|English')) {
+			addTranslationShortcut(translation);
 		}
 	}
 	// turn FR links in green in the content if they point to another FR page
@@ -50,5 +46,15 @@ document.addEventListener('DOMContentLoaded', function() {
 	// TODO : keep the book icon
 	if (historyLink) {
 		historyLink.textContent += ' - ' + lastUpdate;
+	}
+
+	// add a link to dashboard
+	if (userState) {
+		var li = document.createElement('li');
+		var a = document.createElement('a');
+		a.textContent = 'Dashboard';
+		a.href = 'https://developer.mozilla.org/en-US/dashboards/revisions?locale=fr';
+		li.appendChild(a);
+		userState.appendChild(li);
 	}
 });
