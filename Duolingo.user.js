d4494946325e6583036f7ff3685ea2756364c691
Delapouite <delapouite@gmail.com>
2014-07-31 10:22:15 +0200

    added support for GM > 2.*, added UI elts

--- Duolingo.user.js
+++ Duolingo.user.js
@@ -6,31 +6,47 @@
 // @include     https://www.duolingo.com/*
 // @updateURL   https://github.com/Delapouite/userscripts/raw/master/Duolingo.user.js
 // @downloadURL https://github.com/Delapouite/userscripts/raw/master/Duolingo.user.js
-// @version     1.1
+// @version     1.2
+// @grant       GM_setValue
+// @grant       GM_getValue
 // ==/UserScript==
 
-var w = unsafeWindow;
-setTimeout(function() {
+/* globals console, document, setTimeout */
+/* globals GM_setValue, GM_getValue */
+
+// helper
+var $ = document.querySelectorAll.bind(document);
+
+var createSkillsCounters = function(counters) {
+	var h1 = $('h1')[0];
+	h1.textContent += ' ' + counters.finished + ' / ' + counters.total + ' (' + counters.gold + ')';
+};
+
+var scan = function() {
+	try {
 	// tree page only
-	if (!w.$('.sidebar-stats strong').length) return;
+	if (!$('.sidebar-stats strong').length) {
+		return;
+	}
 
 	// local cache
-	var data = GM_getValue('data') || '{}';
-	data = JSON.parse(data);
+	var data = JSON.parse(GM_getValue('data', '{}'));
 
-	var from = w.$('body').attr('class').split('-')[1];
-	var to = w.$('span.flag').attr('class').split('-')[3];
+	var from = document.body.classList[0].split('-')[1];
+	var to = $('span.flag')[0].classList[2].split('-')[1];
 	var counters = {
 		// - 1 to remove the one in sidebar
-		finished: w.$('.skill-icon-strength').length - 1,
-		total: w.$('.skill-icon').length,
-		gold: w.$('.gold').length,
-		xp: w.$('.sidebar-stats strong')[0].textContent,
-		words: w.$('.sidebar-stats strong')[1].textContent,
-		currentLevel: w.$('.level-current')[0].textContent,
-		levelProgress: w.$('.language-progress-bar-small')[0].title.split(' ')[0],
+		finished: $('.skill-icon-strength').length - 1,
+		total: $('.skill-icon').length,
+		gold: $('.gold').length,
+		xp: $('.points strong')[0].textContent,
+		words: $('#word-count')[0].textContent,
+		currentLevel: $('.level-current')[0].textContent,
+		levelProgress: $('.language-progress-bar-small')[0].title.split(' ')[0],
 		date: Date.now()
 	};
+
+	// save
 	if (!data[from]) {
 		data[from] = {};
 	}
@@ -38,14 +54,29 @@ setTimeout(function() {
 	data[from][to] = counters;
 	GM_setValue('data', JSON.stringify(data));
 
+	// enhance UI
+	createSkillsCounters(counters);
+
 	// log
 	Object.keys(data).forEach(function(from) {
 		Object.keys(data[from]).forEach(function(to) {
 			var c = data[from][to];
-			console.log('From', from, 'to', to, c.finished + '/' + c.total, '(' + c.gold +')', c.xp + 'xp', c.words + 'words')
+			console.info(
+				from + ' -> ' + to,
+				c.finished + '/' + c.total + '(' + c.gold + ')',
+				c.xp + 'xp',
+				c.words + 'w',
+				c.currentLevel + 'Lvl(' + c.levelProgress + ')'
+			);
 		});
 	});
 
-	console.log(JSON.stringify(data));
+	console.info(JSON.stringify(data));
 
-}, 6000);
+	} catch (ex) {
+		console.error(ex);
+	}
+};
+
+// wait for full async load
+setTimeout(scan, 6000);
