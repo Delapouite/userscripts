e55aea414d7707e51055ba4495922bc9b0ea150e
Delapouite <delapouite@gmail.com>
2014-10-25 10:09:45 +0200

    added spans clean up to Duo

--- Duolingo.user.js
+++ Duolingo.user.js
@@ -6,7 +6,7 @@
 // @include     https://www.duolingo.com/*
 // @updateURL   https://github.com/Delapouite/userscripts/raw/master/Duolingo.user.js
 // @downloadURL https://github.com/Delapouite/userscripts/raw/master/Duolingo.user.js
-// @version     1.7
+// @version     1.8
 // @grant       GM_setValue
 // @grant       GM_getValue
 // @grant       GM_registerMenuCommand
@@ -26,25 +26,50 @@ var formatDate = function(ts) {
 	return d.getDate() + '-' + (d.getMonth() + 1) + '-' + d.getFullYear();
 };
 
+var formatInfos = function(c) {
+	return [
+		c.finished + '/' + c.total + '(' + c.gold + ')',
+		c.xp + 'xp',
+		c.words + 'w',
+		c.currentLevel + 'Lvl(' + c.levelProgress + ')',
+		formatDate(c.date)
+	];
+};
+
+var createSpan = function(textContent) {
+	var span = document.createElement('span');
+	span.className = 'gm-duo';
+	span.textContent = textContent;
+	return span;
+};
+
+var deleteSkillsCounters = function() {
+	var previousSpans = $('.gm-duo');
+	for (let i = 0; i < previousSpans.length; i++) {
+		previousSpans[i].remove();
+	}
+};
+
 // data already filtered by from
 var createSkillsCounters = function(data, to) {
+	deleteSkillsCounters();
 	// dropdown
 	var languageChoices = $('.language-choice');
-	for (var i = 0; i < languageChoices.length; i++) {
-		var li = languageChoices[i];
-		var d = data[li.dataset.value];
+	for (let i = 0; i < languageChoices.length; i++) {
+		let li = languageChoices[i];
+		let d = data[li.dataset.value];
 		// data not yet computed
 		if (!d) {
 			continue;
 		}
-		var span = document.createElement('span');
-		span.textContent = d.finished + '/' + d.total + '(' + d.gold + ') ' + formatDate(d.date);
+		let span = createSpan(d.finished + '/' + d.total + '(' + d.gold + ') ' + formatDate(d.date));
 		li.appendChild(span);
 	}
-	// in the main title
+	// under the main title
 	var current = data[to];
-	var h1 = $('h1')[0];
-	h1.textContent += ' ' + current.finished + ' / ' + current.total + ' (' + current.gold + ')';
+	var levelText = $('.level-text')[0];
+	var span = createSpan(' ' + formatInfos(current).join(' | '));
+	levelText.appendChild(span);
 };
 
 // to export data
@@ -58,31 +83,13 @@ var log = function(data) {
 
 			var obsolete = Date.now() - c.date > 1000 * 60 * 24 * 7;
 			var logLevel = obsolete ? 'error' : 'info';
-
-			console[logLevel](
-				from + ' -> ' + to,
-				c.finished + '/' + c.total + '(' + c.gold + ')',
-				c.xp + 'xp',
-				c.words + 'w',
-				c.currentLevel + 'Lvl(' + c.levelProgress + ')',
-				formatDate(c.date)
-			);
+			var toLog = [from + ' -> ' + to].concat(formatInfos(c));
+			console[logLevel](toLog);
 		});
 	});
 	console.info('Total xp', totalXp);
 	// export
 	console.info(JSON.stringify(data));
-};
-
-// to handle legacy / new page layouts
-
-var getWords = function() {
-	// new layout
-	var words = $('#word-count');
-	if (words.length) {
-		return words[0].textContent;
-	}
-	return $('.sidebar-stats strong')[1].textContent;
 };
 
 // skills
@@ -98,33 +105,6 @@ var getTotal = function() {
 
 var getGold = function() {
 	return $('.gold').length;
-};
-
-// xps
-
-var getXps = function() {
-	// new layout
-	var xps = $('.points strong');
-	if (xps.length) {
-		return xps[0].textContent;
-	}
-	return $('.sidebar-stats strong')[0].textContent;
-};
-
-var getCurrentLevel = function() {
-	return $('.level-current')[0].textContent;
-};
-
-var getLevelProgress = function() {
-	return $('.language-progress-bar-small')[0].title.split(' ')[0];
-};
-
-var getCurrentXp = function() {
-	return levelProgress.split('/')[0];
-};
-
-var getCeilXp = function() {
-	return levelProgress.split('/')[1];
 };
 
 var scan = function() {
