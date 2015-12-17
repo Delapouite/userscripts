ec94f9ba9a4ea7460e2a6083230bda1240bcd56b
Delapouite <delapouite@gmail.com>
2015-12-17 21:55:01 +0100

    feat(github): add avatars on notif pages

--- GitHub_Notifications.user.js
+++ GitHub_Notifications.user.js
@@ -30,26 +30,49 @@ function decorate (notif, createdAt) {
 	notif.appendChild(s)
 }
 
-var notifications = $('.js-notification')
+// not used yet
+function addCreatedAt () {
+	var notifications = $('.js-notification')
 
-for (let n of notifications) {
-	let href = n.querySelector('.js-notification-target').href
-	let [a, b, c, owner, project, d, issueId] = href.split('/')
-	issueId = issueId.split('#')[0]
-	let notifId = `${owner}/${project}/${issueId}`
+	for (let n of notifications) {
+		let href = n.querySelector('.js-notification-target').href
+		let [a, b, c, owner, project, d, issueId] = href.split('/')
+		issueId = issueId.split('#')[0]
+		let notifId = `${owner}/${project}/${issueId}`
 
-	if (cache[notifId]) {
-		decorate(n, cache[notifId])
-	} else {
-		// TODO API rate limit
-		fetch(`https://api.github.com/repos/${owner}/${project}/issues/${issueId}`)
+		if (cache[notifId]) {
+			decorate(n, cache[notifId])
+		} else {
+			// TODO API rate limit
+			fetch(`https://api.github.com/repos/${owner}/${project}/issues/${issueId}`)
 			.then(res => res.json())
 			.then(res => {
 				if (!res.created_at) return
-				cache[notifId] = res.created_at
+					cache[notifId] = res.created_at
 				save()
 				decorate(n, res.created_at)
 			})
 			.catch(err => console.error(err))
-	} 
+		}
+	}
 }
+
+function addAvatars () {
+	var repos = $('.filter-item')
+
+	for (let r of repos) {
+		let user = r.title.split('/')[0]
+
+		let img = document.createElement('img')
+		img.style.display = 'block'
+		img.style.float = 'left'
+		img.style.height = '1.5em'
+		img.style.marginRight = '0.5em'
+		img.style.width = '1.5em'
+
+		img.src = `https://avatars.githubusercontent.com/${user}`
+		r.insertBefore(img, r.firstChild)
+	}
+}
+
+addAvatars()
